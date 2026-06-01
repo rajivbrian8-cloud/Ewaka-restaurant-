fa81b3697c28a3c768fe60e8251d63675f4f279d
rajivbrian8-cloud <rajivbrian8@gmail.com>
2026-06-01 14:05:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,2 @@
-import streamlit as st
 
-st.set_page_config(page_title="Ewaka Restaurant", page_icon="🍲")
-st.title("Ewaka Restaurant 🍲")
-st.write("Welcome! Our app is under construction. Check back soon.")
 
